ed7bbcc3c4261b939e812111be8729e4b64875f5
Yusuf Taha ATALAY <yusuftahaatalay14@gmail.com>
2020-09-19 22:54:32 +0300

    a screen recorder that records with 20fps

---
+++ main.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+import pyautogui
+
+# display screen resolution
+SCREEN_SIZE = (1920, 1080)
+
+# define the codec
+
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+
+# create the video write object
+# 20 is for FPS value
+out = cv2.VideoWriter("output.avi", fourcc, 20.0, SCREEN_SIZE)
+
+while True:
+    # make a screenshot
+    img = pyautogui.screenshot()
+
+    # convert these pixels to a proper numpy array to work with OpenCV
+    frame = np.array(img)
+
+    # convert colors from BGR to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # write the frame
+    out.write(frame)
+
+    # show the frame
+    cv2.imshow("screenshot", frame)
+
+    # if the user clicks q, it exits
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+# make sure everything is closed when exited
+cv2.destroyAllWindows()
+out.release()
